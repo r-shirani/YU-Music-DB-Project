b758407580e7231104e28a0899101de4059ddc06
R_shirani <127476888+r-shirani@users.noreply.github.com>
2026-01-08 19:58:59 +0330

    Add SQL script to delete all records from tables

---
+++ Delete_tables.sql
@@ -0,0 +1,15 @@
+﻿USE MusicStoreDB;
+GO
+
+EXEC sp_msforeachtable 'ALTER TABLE ? NOCHECK CONSTRAINT all';
+GO
+
+-- پاک کردن تمام رکوردها از همه جدول‌ها
+EXEC sp_msforeachtable 'DELETE FROM ?';
+GO
+
+-- فعال کردن مجدد محدودیت‌ها 
+EXEC sp_msforeachtable 'ALTER TABLE ? WITH CHECK CHECK CONSTRAINT all';
+GO
+
+PRINT 'All tables are now EMPTY. Ready for new data!';
